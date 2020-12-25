89f1ddce191516776177e478daa3352e0120c060
Londers <39859592+Londers@users.noreply.github.com>
2020-12-26 01:48:51 +0300

    Добавлена страница оформления заказа

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { MatStepperModule } from '@angular/material/stepper';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -11,7 +12,10 @@ import { ShopItemComponent } from './shop-item/shop-item.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { CartItemComponent } from './cart-item/cart-item.component';
 import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
-import { checkServerIdentity } from 'tls';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
+import { MatNativeDateModule } from '@angular/material';
+// import { checkServerIdentity } from 'tls';
 
 @NgModule({
   declarations: [
@@ -31,7 +35,12 @@ import { checkServerIdentity } from 'tls';
       { path: '', component: ShopPageComponent, pathMatch: 'full' },
       { path: 'cart', component: CartPageComponent },
       { path: 'checkout', component: CheckoutPageComponent },
-    ])
+    ]),
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ClientApp/src/app/checkout-page/checkout-page.component.ts
+++ ClientApp/src/app/checkout-page/checkout-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
+/**
+ * @title Stepper with editable steps
+ */
 @Component({
-  selector: 'app-checkout-page',
-  templateUrl: './checkout-page.component.html',
-  styleUrls: ['./checkout-page.component.css']
+  selector: 'checkout-page',
+  templateUrl: 'checkout-page.component.html',
+  styleUrls: ['checkout-page.component.css']
 })
 export class CheckoutPageComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phone: ['', Validators.required],
+      personalData: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
-
 }
